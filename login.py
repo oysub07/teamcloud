9b8d559dad37e19f9c4059d17af893863df258ed
SarahParkSehyun <101391544+SarahParkSehyun@users.noreply.github.com>
2022-05-23 00:41:58 +0900

    Update login.py

--- login.py
+++ login.py
@@ -22,3 +22,84 @@ mainFrame.pack()
 
 # email을 저장할 frame(emailFrame)을 main frame에 위치 시킴
 emailFrame = Frame(mainFrame)
+emailFrame.pack()
+lblEmail = Label(emailFrame, text="email", width='8', anchor="w")  # 넓이 8, anchor='w'해서 왼쪽 정렬
+lblEmail.grid(row=1, column=0)  # grid는 격자로 해서 위치 지정!
+email = Entry(emailFrame)
+email.grid(row=1, column=1)
+
+# password을 저장할 frame(emailFrame)을 main frame에 위치 시킴
+passwordFrame = Frame(mainFrame)
+passwordFrame.pack()
+lblPassword = Label(passwordFrame, text="password", width='8', anchor="w")
+lblPassword.grid(row=1, column=0)
+password = Entry(passwordFrame)
+password.grid(row=1, column=1)
+
+global captchaNumber
+
+captchaFrame = Frame(mainFrame)
+captchaFrame.pack(pady=(10, 20))
+
+captcha = Entry(captchaFrame)
+captcha.grid(row=2, column=0)
+
+def regen():
+    global captchaNumber
+    n = str(random.randrange(0, 10 ** MAX_LENGTH))
+    captchaNumber = '0' * (MAX_LENGTH - len(n)) + n
+    imgCaptcha = Label(captchaFrame, text=captchaNumber, width='8', anchor="w")
+    imgCaptcha.grid(row=1, column=0)
+    print(captchaNumber)
+
+
+regen()
+
+
+regenBtn = Button(captchaFrame, text="regen", width='5', command=regen, repeatdelay=1)
+regenBtn.grid(row=1, column=1)
+
+#우선 로그인창의 대략적인 디자인을 구현했습니다. 5/17
+
+
+idlabel=Label(root, text="아이디")
+idlabel.place(x=80, y=130)
+
+
+identry=Entry(root)
+identry.place(x=130, y=130)
+
+pslabel=Label(root, text="비밀번호")
+pslabel.place(x=68, y=160)
+
+psentry=Entry(root)
+psentry.place(x=130, y=160)
+
+seculabel=Label(root, text="보안문자")
+seculabel.place(x=68, y=300)
+
+
+secuentry=Entry(root)
+secuentry.place(x=130, y=300)
+
+secunum=Label(root, text="이 곳에 보안문자 출력")
+secunum.place(x=140, y=210)
+
+
+#아래는 버튼을 나타내기 위해 임시로 사용한 함수입니다.
+
+'''count=0
+
+def countUP():
+    global count
+    count +=1
+    templabel.config(text=str(count))
+    
+templabel=Label(root, text="0")
+templabel.place(x=100, y=260)
+
+rebutton = Button(root, overrelief="solid", width=10, command=countUP,
+                        repeatdelay=1000, repeatinterval=100, text="Regen")
+rebutton.place(x=160, y=260)'''
+
+root.mainloop()
