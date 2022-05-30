a76ce2787c6eb809a47f859073d1210eacf8ebbf
SarahParkSehyun <101391544+SarahParkSehyun@users.noreply.github.com>
2022-05-30 17:05:32 +0900

    Update login.py

--- login.py
+++ login.py
@@ -5,13 +5,13 @@ from tkinter import messagebox
 database = {"email": "password"}
 
 root = Tk()  # main 설정
-root.title("sign in to team10")  # 위에 뜨는 거
+root.title("log in to team10")  # 위에 뜨는 거
 root.geometry("400x400+100+100")  # 넓이 설정
 root.resizable(False, False)  # 크기 조정 가능한지 x축, y축
 
 MAX_LENGTH = 6
 
-title = Label(root, text="Sign In", font='Helvetica 15 bold')
+title = Label(root, text="Log In", font='Helvetica 15 bold')
 # root는 위치 할 곳, text는 표시할 문자,font는 설정
 title.pack(pady='10')
 # pack은 가운데에 위치 시키는 거고, pady는 padding y축 (y축으로 위아래 10씩!)
